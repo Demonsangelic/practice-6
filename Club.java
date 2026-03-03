cdd062ac6a8b9ca67fbd45eba91ed25c090ed62c
Kayla <k_demontigny@hotmail.com>
2026-03-03 13:50:13 -0500

    added codes

--- Club.java
+++ Club.java
@@ -4,9 +4,13 @@
  * @author (your name) 
  * @version 7.0
  */
+import java.util.ArrayList;
 public class Club
 {
     // Define any necessary fields here ...
+    private ArrayList<Membership> members;
+        
+    
     
     /**
      * Constructor for objects of class Club
@@ -14,6 +18,7 @@ public class Club
     public Club()
     {
         // Initialise any fields here ...
+        members = new ArrayList<>();
         
     }
 
@@ -23,6 +28,7 @@ public class Club
      */
     public void join(Membership member)
     {
+        members.add(member);
     }
 
     /**
@@ -31,6 +37,6 @@ public class Club
      */
     public int numberOfMembers()
     {
-        return 0;
+        return members.size();
     }
 }
